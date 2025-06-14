3fbedebdb6880f43731acb680e83d3206b0f2a57
narmingasimli <nerminqasimli210@gmail.com>
2025-06-14 16:34:23 +0000

    update 5 file

--- python-hello_world/5-print_string.py
+++ python-hello_world/5-print_string.py
@@ -1,11 +1,4 @@
 #!/usr/bin/python3
-# Bu script bir metni uc kez ve ardindan ilk 9 karakterini yazdirir.
-# Belirtilen satir sinirlarina ve dongu/kosulsuzluk kosullarina uyar.
-
-str = "Holberton School" # 'str' degiskeni gorev tarafindan saglanir.
-
-# Metni uc kez yazdirir, ardindan yeni bir satir gelir (print icin varsayilan).
+str = "Holberton School"
 print(str * 3)
-
-# Metnin ilk 9 karakterini yazdirir, ardindan yeni bir satir gelir.
 print(str[:9])

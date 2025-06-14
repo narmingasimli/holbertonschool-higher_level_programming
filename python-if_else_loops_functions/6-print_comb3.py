ecd185c2dedcb7c2581423c2678b8a7b1c04fa3c
narmingasimli <nerminqasimli210@gmail.com>
2025-06-14 19:55:36 +0000

    update 6 add file

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-for i in range(10):  # First digit from 0 to 9
-    for j in range(i + 1, 10):  # Second digit from i+1 to 9 (must be larger)
-        num_combination = i * 10 + j  # Combine digits into a two-digit number
+for i in range(10):
+    for j in range(i + 1, 10):
         if i == 8 and j == 9:
-            print("{:02d}".format(num_combination), end="")
+            print("{:02d}".format(i * 10 + j), end="")
         else:
-            print("{:02d}".format(num_combination), end=", ") # No trailing whitespace here
+            print("{:02d}".format(i * 10 + j), end=", ")
 print("")

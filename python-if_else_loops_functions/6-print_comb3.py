b4330259e58bb773ac3b6896daca6167564d6104
narmingasimli <nerminqasimli210@gmail.com>
2025-06-14 19:51:45 +0000

    create 6 file

---
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+for i in range(10):  # First digit from 0 to 9
+    for j in range(i + 1, 10):  # Second digit from i+1 to 9 (must be larger)
+        num_combination = i * 10 + j  # Combine digits into a two-digit number
+
+        # Check if it's the last combination (89)
+        if i == 8 and j == 9:
+            print("{:02d}".format(num_combination), end="")
+        else:
+            print("{:02d}".format(num_combination), end=", ")
+
+print("") 

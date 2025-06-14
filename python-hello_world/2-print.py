38869a4676ba12109ff4bf6d670bf9258c1b749f
narmingasimli <nerminqasimli210@gmail.com>
2025-06-14 14:56:23 +0000

    Adds 2-print.py for Holberton task

---
+++ python-hello_world/2-print.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# 2-print.py
+
+# This script prints a specific string followed by a new line.
+# The print() function in Python 3 automatically adds a newline character
+# at the end of the output unless specified otherwise.
+print("Programming is like building a multilingual puzzle")

93c4612517037c83248f8b7c0520a57571bf332c
narmingasimli <nerminqasimli210@gmail.com>
2025-06-14 18:47:34 +0000

    Fix Pycodestyle E501 errors in 1-last_digit.py

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -2,15 +2,11 @@
 import random
 number = random.randint(-10000, 10000)
 
-# Ədədin son rəqəmini hesabla.
-# Müsbət ədədlər üçün number % 10, mənfi ədədlər üçün number % -10 istifadə edirik
-# ki, son rəqəmin işarəsi də ədədin işarəsi ilə eyni olsun.
 if number < 0:
     last_digit = number % -10
 else:
     last_digit = number % 10
 
-# Şərtləri yoxla və çıxışı çap et.
 if last_digit > 5:
     print(f"Last digit of {number} is {last_digit} and is greater than 5")
 elif last_digit == 0:

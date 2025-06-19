5f4dcc6e1e33a9aebba0d43f169215702180b5c2
narmingasimli <nerminqasimli210@gmail.com>
2025-06-19 07:10:36 +0000

    create  0-add.py file

---
+++ python-import_modules/0-add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# add_0.py
+def add(a, b):
+    """My addition function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a + b
+    """
+    return (a + b)

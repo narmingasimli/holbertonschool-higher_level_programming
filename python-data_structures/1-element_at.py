e3fc72573d5c10a37ff7fd3c1353bb91f05d7cab
narmingasimli <nerminqasimli210@gmail.com>
2025-06-21 16:06:31 +0000

    1-element_at.py

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -2,10 +2,7 @@
 def element_at(my_list, idx):
     if idx < 0:
         return None
-    elif idx > len(my_list):
+    elif idx >= len(my_list):
         return None
     else:
         return my_list[idx]
-my_list = [1, 2, 3, 4, 5]
-idx = 3
-print("Element at index {:d} is {}".format(idx, element_at(my_list, idx)))

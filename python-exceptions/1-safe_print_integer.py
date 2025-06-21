c43f153d5e040a063d454c9293bffa16f6883e6b
narmingasimli <nerminqasimli210@gmail.com>
2025-06-21 18:56:34 +0000

    add 1-safe_print_integer.py file

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        if value == int:
-            return value
-    except TypeError:
-        pass
-    print()
+        print("{:d}".format(value))
+        return True
+    except (ValueError, TypeError):
+        return False

a4f2765491e4b46c08f31208dd9b5cc739ba628b
narmingasimli <nerminqasimli210@gmail.com>
2025-06-30 12:15:29 +0000

    add file

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+Rectangle = __import__('9-rectangle').Rectangle
+
+r = Rectangle(3, 5)
+
+print(r)
+print(r.area())
+root@bfa0ab48bbe641e1864c7e5e2469c878-2377118072:~/holbertonschool-higher_level_programming/python-inheritance# cat 8-rectangle.py 
+#!/usr/bin/python3
+"""a class that is a subclass and initialises an object"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    ''' a class Rectangle that inherits from BaseGeometry (7-base_geometry.py)
+        Instantiation with width and height: def __init__(self, width, height)
+        width and height must be private. No getter or setter
+        width and height must be positive integers, validated
+        by integer_validator'''
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        return f"[Rectangle] {self.__width}/{self.__height}"        

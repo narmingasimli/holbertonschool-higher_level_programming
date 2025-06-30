8d119fdc48ec01268b049491ea1aab2544245078
narmingasimli <nerminqasimli210@gmail.com>
2025-06-30 12:23:50 +0000

    add 11-square.py file update

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -2,14 +2,10 @@
 # 11-square.py
 """Defines a Rectangle subclass Square."""
 Rectangle = __import__('9-rectangle').Rectangle
-
-
 class Square(Rectangle):
     """Represent a square."""
-
     def __init__(self, size):
         """Initialize a new square.
-
         Args:
             size (int): The size of the new square.
         """

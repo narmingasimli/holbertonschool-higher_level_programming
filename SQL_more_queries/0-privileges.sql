65e5183e5505d15e714bfb07093784e5e8fed99f
narmingasimli <nerminqasimli210@gmail.com>
2025-07-10 08:23:34 +0000

    add and update  0-privileges.sql file

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,5 +1,3 @@
--- user_0d_1 and user_0d_2:
-SELECT 
-	user_0d_1,
-	user_0d_2
-FROM localhost;
+-- SHOW user_0d_1 and user_0d_2 in MySQL:
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost'; 
